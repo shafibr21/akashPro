3bc03daa35763469de33b84abe82047c1bd4af84
Abdur Rahman Shafi <111105311+shafibr21@users.noreply.github.com>
2024-10-26 23:32:25 +0600

    ad add done

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -4,10 +4,10 @@ const productSchema = new mongoose.Schema({
   name: { type: String, require: true },
   description: { type: String, require: true },
   price: { type: Number, require: true },
-  image: { type: Array, require: true },
+  images: { type: Array, require: true },
   category: { type: String, require: true },
   subCategory: { type: String, require: true },
-  size: { type: Array, require: true },
+  sizes: { type: Array, require: true },
   bestSeller: { type: Boolean },
   date: { type: Number, require: true },
 });
